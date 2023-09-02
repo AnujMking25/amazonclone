796b439af0c18e2a0be43d0bd8a1915af055cc34
AnujMking25 <anuj170499@gmail.com>
2023-09-02 14:10:34 +0530

    carousel is done with video

---
+++ src/component/Home.jsx
@@ -0,0 +1,15 @@
+import Carousel from "./Carousel"
+
+
+const Home = () => {
+  return (
+    <div className="bg-amazonclone-background">
+    <div className="min-w-[800px] max-w-[1500px] m-auto bg-purple-500">
+    Home
+    <Carousel/>
+    </div>
+    </div>
+    )
+}
+
+export default Home
