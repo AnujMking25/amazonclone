222f6ee03dacbc6514f7c5a0507f24848ead79df
AnujMking25 <anuj170499@gmail.com>
2023-09-05 20:34:31 +0530

    front-end is done (its backend Time)

--- src/component/Page/Checkout/Checkout.jsx
+++ src/component/Page/Checkout/Checkout.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
+    <div className="h-screen bg-amazonclone-background">
+      <div className="min-w-[1000px] max-w-[1500px] m-auto pt-8">
+        <div className="grid grid-cols-8 gap-10">
+          {/* Products */}
+          <div className="col-span-6 bg-white">
+            <div className="text-2xl xl:text-3xl m-4">Shopping Cart</div>
+           
+            <div className="text-lg xl:text-xl text-right mb-4 mr-4">
+             
+              <span className="font-semibold">
+               
+              </span>
+            </div>
+          </div>
+          {/* Checkout */}
+          <div className="col-span-2 bg-white rounded h-[250px] p-7">
+            <div className="text-xs xl:text-sm text-green-800 mb-2">
+              Your order qualifies for{" "}
+              <span className="font-bold">FREE DELIVERY</span>. Delivery Details
+            </div>
+            <div className="text-base xl:text-lg mb-4">
+             
+              <span className="font-semibold">
+
+              </span>
+            </div>
+            <button >Proceed to Checkout</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
